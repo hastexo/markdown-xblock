964a0d64f71b5790ebd9b12e4464a9d1544e64d7
Adolfo R. Brandes <arbrandes@arbrand.es>
2016-08-11 10:49:58 -0300

    Load markdown from static store at runtime
    It turns out that accessing the filesystem directly from an XBlock is
    not easily done, specially during XML parse.  Thus, load markdown files
    at runtime.
    
    Verified to work in the workbench.

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -1,19 +1,12 @@
 import logging
 import markdown2
-
-from fs.osfs import OSFS
-from fs.errors import ResourceNotFoundError
-from path import Path as path
+import textwrap
 
 from xblock.core import XBlock
 from xblock.fields import Scope, String
 from xblock.fragment import Fragment
 from xblockutils.resources import ResourceLoader
 from xblockutils.studio_editable import StudioEditableXBlockMixin
-from xmodule.contentstore.content import StaticContent
-from xmodule.contentstore.django import contentstore
-from xmodule.exceptions import NotFoundError
-from opaque_keys import InvalidKeyError
 
 log = logging.getLogger(__name__)
 loader = ResourceLoader(__name__)
@@ -26,118 +19,105 @@ class MarkdownXBlock(StudioEditableXBlockMixin, XBlock):
     """
     display_name = String(
         help="This name appears in the horizontal navigation at the top of the page.",
-        display_name="Display Name",
-        scope=Scope.settings,
-        default=""
-    )
+        default="",
+        scope=Scope.settings)
     content = String(
-        help="Markdown content to display for this module",
+        help="Markdown content to display for this module.",
         default=u"",
         scope=Scope.content)
+    filename = String(
+        help="Relative path to a markdown file in the static store.",
+        default="",
+        scope=Scope.content)
+
+    editable_fields = (
+        'display_name',
+        'content',
+        'filename')
+
+    has_author_view = True
 
     @classmethod
     def parse_xml(cls, node, runtime, keys, id_generator):
         """
-        Parse the XML for a markdown block.
+        Parses the source XML in a way that preserves indenting, needed for markdown.
 
         """
         block = runtime.construct_xblock_from_class(cls, keys)
 
-        # Load the data
-        filename = node.get('filename')
-        if filename is None:
-            content = node.text
-        else:
-            url_name = node.get('url_name')
-            location = id_generator.create_definition(node.tag, url_name)
-            pointer_path = "markdown/{url_path}".format(
-                url_path=location.name.replace(':', '/')
-            )
-            base = path(pointer_path).dirname()
-            filepath = "{base}/{filename}.md".format(
-                    base=base,
-                    filename=filename)
+        # The base implementation: child nodes become child blocks.
+        # Or fields, if they belong to the right namespace.
+        for child in node:
+            if child.tag is etree.Comment:
+                continue
+            qname = etree.QName(child)
+            tag = qname.localname
+            namespace = qname.namespace
 
-            # TODO: resource_fs comes from
-            # edx-platform/common/lib/xmodule/xmodule/modulestore/xml.py.  Need
-            # to figure out how to get these directories from an XBlock, as
-            # opposed to an XModule.  Might be best to import resource_fs
-            # directly from the runtime.
-            resource_fs = OSFS(xmlstore.data_dir / course_dir)
-            try:
-                with resource_fs.open(filepath) as infile:
-                    text = infile.read().decode('utf-8')
-            except (ResourceNotFoundError) as err:
-                msg = 'Unable to load file contents at path {0}: {1} '.format(
-                    filepath, err)
-                raise Exception(msg), None, sys.exc_info()[2]
+            if namespace == XML_NAMESPACES["option"]:
+                cls._set_field_if_present(block, tag, child.text, child.attrib)
+            else:
+                block.runtime.add_node_as_child(block, child, id_generator)
 
-        if content:
-            content = content.strip()
-            if content:
-                block.content = content
+        # Attributes become fields.
+        for name, value in node.items():
+            cls._set_field_if_present(block, name, value, {})
+
+        # Text content becomes "content", if such a field exists.
+        if "content" in block.fields and block.fields["content"].scope == Scope.content:
+            text = node.text
+            if text:
+                # Fix up whitespace.
+                if text[0] == "\n":
+                    text = text[1:]
+                text.rstrip()
+                text = textwrap.dedent(text)
+                if text:
+                    block.content = text
 
         return block
 
-    def definition_to_xml(self, resource_fs):
+    def student_view(self, context=None):
         """
-        Write <markdown filename="" [meta-attrs="..."]> to filename.xml,
-        and the markdown string to filename.md.
-
-        """
-        # Write markdown to file, return an empty tag
-        pathname = self.url_name.replace(':', '/')
-        filepath = u'markdown/{pathname}.md'.format(
-            pathname=pathname
-        )
-
-        resource_fs.makedir(os.path.dirname(filepath), recursive=True, allow_recreate=True)
-        with resource_fs.open(filepath, 'w') as filestream:
-            markdown_data = self.data.encode('utf-8')
-            filestream.write(markdown_data)
-
-        # write out the relative name
-        relname = path(pathname).basename()
-
-        elt = etree.Element('markdown')
-        elt.set("filename", relname)
-        return elt
-
-    def add_xml_to_node(self, node):
-        """
-        Set attributes and children on `node` to represent ourselves as XML.
-
-        We parse our HTML content, and graft those nodes onto `node`.
+        The student view of the MarkdownXBlock.
 
         """
-        xml = "<html_demo>" + self.content + "</html_demo>"
-        html_node = etree.fromstring(xml)
+        if self.filename:
+            # These can only be imported when the XBlock is running on the LMS.
+            # Do it at runtime so that the workbench is usable for regular XML
+            # content.
+            from xmodule.contentstore.content import StaticContent
+            from xmodule.contentstore.django import contentstore
+            from xmodule.exceptions import NotFoundError
+            from opaque_keys import InvalidKeyError
+            try:
+                loc = StaticContent.compute_location(course_id, self.filename)
+                asset = contentstore().find(loc)
+                content = asset.data
+            except (NotFoundError, InvalidKeyError):
+                pass
+        else:
+            content = self.content
 
-        node.tag = html_node.tag
-        node.text = html_node.text
-        for child in html_node:
-            node.append(child)
-
-    def fallback_view(self, context=None):
-        """
-        The primary view of the MarkdownXBlock, shown to students when viewing
-        courses.
-        """
-        # Load the markdown file and convert it
-        content = None
-        try:
-            loc = StaticContent.compute_location(course_id, self.path)
-            asset = contentstore().find(loc)
-            content = markdown2.markdown(asset.data)
-        except (NotFoundError, InvalidKeyError):
-            pass
+        if content:
+            html = markdown2.markdown(content)
 
         # Render the HTML template
-        html_context = {'content': content}
-        html = loader.render_template('templates/main.html', html_context)
+        context = {'content': html}
+        html = loader.render_template('templates/main.html', context)
         frag = Fragment(html)
 
         return frag
+
+    def author_view(self, context=None):
+        """
+        Studio preview.
+
+        """
+        if self.filename:
+            return Fragment(u"<em>If filename is set, no preview is available.</em>")
+        else:
+            return self.student_view(context)
 
     @staticmethod
     def workbench_scenarios():
@@ -145,7 +125,29 @@ class MarkdownXBlock(StudioEditableXBlockMixin, XBlock):
         return [
             ("MarkdownXBlock",
              """<vertical_demo>
-                <markdown/>
+                <markdown>
+                    # This is an h1
+
+                    ## This is an h2
+
+                    This is a regular paragraph.
+
+                        This is a code block.
+
+                    > This is a blockquote.
+
+                    * This is
+                    * an unordered
+                    * list
+
+                    1. This is
+                    1. an ordered
+                    1. list
+
+                    [Link to cat](http://i.imgur.com/3xVUnyA.jpg)
+
+                    ![Cat](http://i.imgur.com/3xVUnyA.jpg)
+                </markdown>
                 </vertical_demo>
              """),
         ]
